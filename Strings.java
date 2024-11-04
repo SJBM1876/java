83825ea71b1fb1840d1f97a4f25a6f203a5ba1ef
SJBM1876 <137792828+SJBM1876@users.noreply.github.com>
2024-11-04 13:23:02 +0000

    Using string variables.

---
+++ Strings.java
@@ -0,0 +1,14 @@
+public class Strings {
+    public static void main(String[] args) {
+        int myInt = 27;
+
+        String text = "Hi";
+        String blank = " ";
+        String name = "Bob";
+        String greeting = text + blank + name;
+
+        System.out.println(greeting);
+        System.out.println("My name is " + name + " and I am " + myInt + " years old.");
+
+    }
+}
