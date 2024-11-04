747a0fad005fa78cb696e3037a812409b7222fcb
SJBM1876 <137792828+SJBM1876@users.noreply.github.com>
2024-11-04 12:14:53 +0000

    Using variables.

---
+++ Application.java
@@ -0,0 +1,27 @@
+public class Application {
+    public static void main(String[] args) {
+        int myNumber = 88;
+        short myShort = 847;
+        long myLong = 568878;
+
+        double myDouble = 8.97807;
+        float myFloat = 0.934f;
+
+        char myChar = 'c';
+
+        boolean myBool = false;
+
+        byte myByte = 127;
+
+
+
+        System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBool);
+        System.out.println(myByte);
+    }
+}
