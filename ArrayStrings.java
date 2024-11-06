6a331504ce68fa6b460bd318094965f037b3aa9d
SJBM1876 <137792828+SJBM1876@users.noreply.github.com>
2024-11-06 11:19:59 +0000

    Arrays and strings.

---
+++ ArrayStrings.java
@@ -0,0 +1,8 @@
+public class ArrayStrings {
+    public static void main(String[] args) {
+        String[] footballClubs = {"Port Vale", "Arsenal", "Leeds United", "Wigan Athletic"};
+
+        System.out.println(footballClubs[0]);
+
+    }
+}
